d970da9567ea377af34060ae597dc9318ca00658
iamadahn <vendonism16@yandex.ru>
2025-02-14 22:16:28 +0300

    drivers: usart: rework

--- src/drivers/usart/usart.c
+++ src/drivers/usart/usart.c
@@ -1,63 +1,148 @@
 #include "usart.h"
-#include "stm32f1xx_ll_usart.h"
-#include "stm32f1xx_ll_utils.h"
-#include <stdint.h>
-#include <stddef.h>
-#include <string.h>
 #include "dwt.h"
 
-void
-usart_config(usart_t* usart) {
-    LL_USART_SetTransferDirection(usart->instance, LL_USART_DIRECTION_TX_RX);
-    LL_USART_ConfigCharacter(usart->instance, LL_USART_DATAWIDTH_8B, LL_USART_PARITY_NONE, LL_USART_STOPBITS_1);
-    LL_USART_SetBaudRate(usart->instance, usart->periph_clock_freq, usart->baudrate); 
-    LL_USART_Enable(usart->instance);
-}
+#include <stm32f1xx_ll_bus.h>
+#include <stm32f1xx_ll_usart.h>
 
-uint8_t
-usart_blck_transmit(usart_t* usart, void* data, uint32_t size, uint16_t timeout) {
-    if (data == NULL) {
-        return 1;
+#include <stddef.h>
+
+static int8_t usart_async_fduplex_init(struct usart_config *usart);
+
+int8_t usart_init(struct usart_config *usart)
+{
+    if (usart == NULL)
+        return -1;
+
+    if (!usart->async_mode)
+        return -2; // Not supported
+    
+    int8_t ret = usart_async_fduplex_init(usart);
+    if (ret != 0)
+        return ret;
+
+    if (usart->irq_mode) {
+        switch ((intptr_t)usart->self) {
+            case (intptr_t)USART1:
+                NVIC_SetPriority(USART1_IRQn, usart->irq_priority);
+                NVIC_EnableIRQ(USART1_IRQn);
+                LL_USART_EnableIT_RXNE(USART1);
+                break;
+
+            case (intptr_t)USART2:
+                NVIC_SetPriority(USART2_IRQn, usart->irq_priority);
+                NVIC_EnableIRQ(USART2_IRQn);
+                LL_USART_EnableIT_RXNE(USART2);
+                break;
+
+            case (intptr_t)USART3:
+                NVIC_SetPriority(USART3_IRQn, usart->irq_priority);
+                NVIC_EnableIRQ(USART3_IRQn);
+                LL_USART_EnableIT_RXNE(USART3);
+                break;
+        }
     }
 
-    uint8_t* msg = (uint8_t*)data;
+    return ret;
+}
 
-    uint32_t timeout_counter = 0, timeout_usec = timeout * 1000;
+int8_t usart_async_fduplex_init(struct usart_config *usart)
+{
+    if (usart->self == NULL)
+        return -1;
+
+    switch ((intptr_t)usart->self) {
+        case (intptr_t)USART1:
+            LL_APB2_GRP1_EnableClock(LL_APB2_GRP1_PERIPH_USART1);
+            break;
+
+        case (intptr_t)USART2:
+            LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART2);
+            break;
+
+        case (intptr_t)USART3:
+            LL_APB1_GRP1_EnableClock(LL_APB1_GRP1_PERIPH_USART3);
+            break;
+
+        default:
+            return -1;
+            break;
+    }
+
+    LL_USART_SetTransferDirection(usart->self, usart->transfer_direction);
+    LL_USART_ConfigCharacter(usart->self, usart->data_width, usart->parity, usart->stopbits);
+    LL_USART_SetBaudRate(usart->self, usart->apb_bus_freq, usart->baudrate);
+    LL_USART_ConfigAsyncMode(usart->self);
+    LL_USART_Enable(usart->self);
+
+    (void)usart->self->DR;
+
+    return 0;
+}
+
+int8_t usart_block_transmit(struct usart_config *usart, void *data, uint32_t size, uint32_t timeout)
+{
+    if (data == NULL)
+        return -1;
+
+    uint8_t *msg = (uint8_t*)data;
+    uint32_t timeout_counter = 0;
 
     for (uint32_t i = 0; i < size; i++) {
-
-        while (!LL_USART_IsActiveFlag_TXE(USART1)) {
-            DWT_Delay(1);
-            if (++timeout_counter == timeout_usec) {
+        while (!LL_USART_IsActiveFlag_TXE(usart->self)) {
+            dwt_delay(1);
+            if (timeout_counter > timeout)
                 return 1;
-            }
+            timeout_counter++;
         }
 
-        LL_USART_TransmitData8(USART1, msg[i]);
+        LL_USART_TransmitData8(usart->self, msg[i]);
+        while (!LL_USART_IsActiveFlag_TC(usart->self));
     }
 
     return 0;
 }
 
-uint8_t
-usart_blck_receive(usart_t* usart, void* buf, uint32_t size, uint16_t timeout) {
-    if (buf == NULL) {
-        return 1;
-    }
+int8_t usart_block_receive(struct usart_config *usart, void *buf, uint32_t size, uint32_t timeout)
+{
+    if (buf == NULL)
+        return -1;
 
-    uint8_t* msg = (uint8_t*)buf;
+    uint8_t *msg = (uint8_t*)buf;
 
-    uint32_t timeout_counter = 0, timeout_usec = timeout * 1000;
+    uint32_t timeout_counter = 0;
 
     for (uint32_t i = 0; i < size; i++) {
-        while (!LL_USART_IsActiveFlag_RXNE(USART1)) {
-            DWT_Delay(1);
-            if (++timeout_counter == timeout_usec) {
+        while (!LL_USART_IsActiveFlag_RXNE(usart->self)) {
+            dwt_delay(1);
+            if (timeout_counter > timeout)
                 return 1;
-            }
+            timeout_counter++;
         }
-        msg[i] = LL_USART_ReceiveData8(USART1);
+
+        msg[i] = LL_USART_ReceiveData8(usart->self);
     }
 
     return 0;
 }
+
+__attribute__((weak)) void usart1_irq_handler(void){}
+__attribute__((weak)) void usart2_irq_handler(void){}
+__attribute__((weak)) void usart3_irq_handler(void){}
+
+#ifndef USE_HAL_DRIVER
+void USART1_IRQHandler(void)
+{
+    usart1_irq_handler();
+}
+
+void USART2_IRQHandler(void)
+{
+    usart2_irq_handler();
+}
+
+void USART3_IRQHandler(void)
+{
+    usart3_irq_handler();
+}
+#endif
+
--- src/drivers/usart/usart.h
+++ src/drivers/usart/usart.h
@@ -1,16 +1,27 @@
 #ifndef USART_H_
 #define USART_H_
 
-#include "stm32f1xx_ll_usart.h"
+#include <stm32f1xx_ll_usart.h>
 
-typedef struct {
-    USART_TypeDef* instance;
+struct usart_config {
+    USART_TypeDef *self;
+    uint32_t apb_bus_freq;
     uint32_t baudrate;
-    uint32_t periph_clock_freq;
-} usart_t;
+    uint32_t transfer_direction;
+    uint32_t data_width;
+    uint32_t parity;
+    uint32_t stopbits;
+    uint32_t irq_priority;
+    uint8_t async_mode;
+    uint8_t irq_mode;
+};
 
-void usart_config(usart_t* usart);
-uint8_t usart_blck_transmit(usart_t* usart, void* data, uint32_t size, uint16_t timeout);
-uint8_t usart_blck_receive(usart_t* usart, void* buf, uint32_t size, uint16_t timeout);
+int8_t usart_init(struct usart_config *usart);
+int8_t usart_block_transmit(struct usart_config *usart, void *data, uint32_t size, uint32_t timeout);
+int8_t usart_block_receive(struct usart_config *usart, void *buf, uint32_t size, uint32_t timeout);
+
+void usart1_irq_handler(void);
+void usart2_irq_handler(void);
+void usart3_irq_handler(void);
 
 #endif
